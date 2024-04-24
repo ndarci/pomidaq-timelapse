26fcbe8c64c8a17c4eda33d1d10941782a13c713
Niko Darci-Maher <niko.darcimaher@gmail.com>
2024-04-24 15:22:15 -0400

    implementing filename storage structure

--- timelapse/timelapse.py
+++ timelapse/timelapse.py
@@ -22,7 +22,7 @@ from miniscope import Miniscope, ControlKind
 from mscopesetup import setup_miniscope
 from mscopecontrol import set_led, set_focus, set_gain
 
-def generate_file_path(image_dir, snapshot_num, z_index, focus, led, gain):
+def generate_file_path(image_dir, time_step, z_index, focus, led, gain):
     '''Generate an absolute path for a single image, using all the info associated with that image. Create the z directory if needed.'''
 
     # TODO: get a precise timestamp of when the image was taken, store in name or metadata
@@ -39,7 +39,7 @@ def generate_file_path(image_dir, snapshot_num, z_index, focus, led, gain):
         os.makedirs(os.path.join(image_dir, z_dir))
 
     # get all the relevant image info into the final filename
-    img_name = 'miniscope_t' + str(snapshot_num) + '_z' + focus_str + '_led' + str(led) + '_gain' + str(gain) + '.jpg'
+    img_name = 'miniscope_t' + str(time_step) + '_z' + focus_str + '_led' + str(led) + '_gain' + str(gain) + '.jpg'
     
     return os.path.join(image_dir, z_dir, img_name)
 
@@ -57,49 +57,55 @@ def take_photo(m, image_fn, nbuffer_frames = 50):
             nframes += 1
     cv2.imwrite(image_fn, frame)
 
-def take_zstack(m, image_dir, snapshot_num, zparams, led, gain):
+def take_zstack(m, image_dir, time_step, zparams, led, gain, filenames):
     '''Shoot a z-stack of photos with the Miniscope'''
     current_focus = zparams['start']
     z_index = 0
     while current_focus <= zparams['end']:
+        this_file_path = generate_file_path(image_dir, time_step, z_index, current_focus, led, gain)
         set_focus(m, current_focus)
-        take_photo(m, generate_file_path(image_dir, snapshot_num, z_index, current_focus, led, gain))
+        take_photo(m, this_file_path)
         current_focus += zparams['step']
         z_index += 1
+        filenames[current_focus].append(this_file_path)
+    return filenames
 
 def print_hline():
     print('--------------------')
 
-def shoot_timelapse(m, image_dir, zparams, total_snapshots, period_sec, excitation_strength = 20, gain = 0):
+def shoot_timelapse(m, image_dir, zparams, total_timesteps, period_sec, excitation_strength = 20, gain = 0):
     '''Shoot a timelapse, which will be a set of folders for each z-level, full of image files at each time point.'''
 
     print()
     print_hline()
     print("Starting time lapse recording.")
-    print("\tTotal snapshots = " + str(total_snapshots))
+    print("\tTotal timesteps = " + str(total_timesteps))
     print("\tPeriod (sec) = " + str(period_sec))
     print("\tZ-Stack settings = " + str(zparams))
     print_hline()
 
-    nsnapshots = 0
-    filenames = [None] * total_snapshots
+    timestep = 0
+    filenames = {}
+    for z in range(zparams['start'], zparams['end']+1, zparams['step']):
+        filenames[z] = []
+
     # time lapse loop
-    while nsnapshots < total_snapshots:
+    while timestep < total_timesteps:
         print()
-        print("Taking z-stack " + str(nsnapshots) + " ...")
+        print("Taking z-stack " + str(timestep) + " ...")
         
         # turn the LED on
         set_led(m, excitation_strength)
 
         # take a z-stack at the current state
-        take_zstack(m, image_dir, nsnapshots, zparams, excitation_strength, gain)
+        filenames = take_zstack(m, image_dir, timestep, zparams, excitation_strength, gain, filenames)
         
         # turn the LED off
         set_led(m, 0)
         
-        nsnapshots += 1
+        timestep += 1
 
-        if nsnapshots < total_snapshots:
+        if timestep < total_timesteps:
             print()
             print("Waiting", period_sec, "seconds to take next z-stack ...")
             time.sleep(period_sec)
@@ -115,16 +121,16 @@ def shoot_timelapse(m, image_dir, zparams, total_snapshots, period_sec, excitati
 
     return filenames
 
-def merge_snapshots(vid_dir, vid_fn_list, ffmpeg_path, merged_vid_name):
-    '''Use ffmpeg to merge the miniscope snapshots into a single video'''
-    # write the list of snapshot filenames to a text file for ffmpeg
-    merge_list = open(os.path.join(vid_dir, 'merge_file_names.txt'), 'w')
-    merge_list.write('# miniscope snapshots\n')
-    for filename in vid_fn_list:
-        merge_list.write("file " + "'" + filename + "'\n")
-    merge_list.close()
-    # call ffmpeg in concat mode on the list of snapshot files
-    subprocess.call([ffmpeg_path, '-f', 'concat', '-safe', '0', '-i', os.path.join(vid_dir, 'merge_file_names.txt'), '-c', 'copy', os.path.join(vid_dir, merged_vid_name)])
+# def merge_timelapse(vid_dir, vid_fn_list, ffmpeg_path, merged_vid_name):
+#     '''Use ffmpeg to merge the miniscope images into a single video'''
+#     # write the list of filenames to a text file for ffmpeg
+#     merge_list = open(os.path.join(vid_dir, 'merge_file_names.txt'), 'w')
+#     merge_list.write('# miniscope images\n')
+#     for filename in vid_fn_list:
+#         merge_list.write("file " + "'" + filename + "'\n")
+#     merge_list.close()
+#     # call ffmpeg in concat mode on the list of image files
+#     subprocess.call([ffmpeg_path, '-f', 'concat', '-safe', '0', '-i', os.path.join(vid_dir, 'merge_file_names.txt'), '-c', 'copy', os.path.join(vid_dir, merged_vid_name)])
 
 def main():
     # create new Miniscope instance
@@ -138,16 +144,18 @@ def main():
     set_focus(mscope, 0)
     set_gain(mscope, 0)
 
-    # run timelapse loop and save all snapshots
+    # run timelapse loop and save all images
     date_sec = datetime.now().strftime("%Y%m%d_%H%M%S")
     image_dir_now = BASE_IMAGE_DIRNAME + '_' + str(date_sec)
     os.makedirs(image_dir_now, exist_ok = True)
     zstack_parameters = {'start': -120, 'end': 120, 'step': 120}
-    image_filename_list = shoot_timelapse(mscope, image_dir = image_dir_now, zparams = zstack_parameters, total_snapshots = 2, period_sec = 1)
+    image_filename_dict = shoot_timelapse(mscope, image_dir = image_dir_now, zparams = zstack_parameters, total_timesteps = 2, period_sec = 1)
 
-    # merge snapshots into a single video
+    print(image_filename_dict)
+
+    # merge images into a time lapse video
     # final_merged_video_name = 'miniscope_timelapse_merged_' + date_sec + '.mp4'
-    # merge_snapshots(video_dirname, video_filename_list, ffmpeg_path, final_merged_video_name)
+    # merge_timelapse(video_dirname, video_filename_list, ffmpeg_path, final_merged_video_name)
 
     # disconnect from scope 
     mscope.disconnect()
